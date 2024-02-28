10da53090fe1a8106486eb2abc4df90478bacd40
fapethedev <mpetrivlin@gmail.com>
2024-02-28 22:12:14 +0000

    Setup SpringBoot app

---
+++ src/main/java/com/fapethedev/tendance/TendanceApplication.java
@@ -0,0 +1,13 @@
+package com.fapethedev.tendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TendanceApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(TendanceApplication.class, args);
+	}
+}
