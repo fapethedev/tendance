0a940fe567c6ce26fc5d1dcb357961419eae8d0f
fapethedev <mpetrivlin@gmail.com>
2024-04-03 10:50:35 +0000

    UPDATE UserRepository ADD new methods

--- src/main/java/com/fapethedev/tendance/users/repositories/UserRepository.java
+++ src/main/java/com/fapethedev/tendance/users/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.fapethedev.tendance.users.repositories;
 
 import com.fapethedev.tendance.users.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,11 @@ public interface UserRepository extends JpaRepository<User, UUID>
 {
     @Query("select u from User u where u.identity.email =:email")
     Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("SELECT COUNT(1) FROM User U WHERE U.identity.email =:email")
+    Long countByEmail(@Param("email") String email);
+
+    @Modifying
+    @Query("UPDATE User U SET U.account.active=false WHERE U.identity.email =:email")
+    void deleteByUsername(@Param("email") String username);
 }
