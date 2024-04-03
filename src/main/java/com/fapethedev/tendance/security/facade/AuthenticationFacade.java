7892bdba4e7704bc778db08ba57e3bd7759770c4
fapethedev <mpetrivlin@gmail.com>
2024-04-03 10:34:34 +0000

    UPDATE AuthenticationFacade remove unnecessary method

--- src/main/java/com/fapethedev/tendance/security/facade/AuthenticationFacade.java
+++ src/main/java/com/fapethedev/tendance/security/facade/AuthenticationFacade.java
@@ -1,13 +1,8 @@
 package com.fapethedev.tendance.security.facade;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
 
 public interface AuthenticationFacade
 {
     Authentication getAuthentication();
-
-    void setAuthentication(String username, String password, Collection<? extends GrantedAuthority> authorities);
 }
