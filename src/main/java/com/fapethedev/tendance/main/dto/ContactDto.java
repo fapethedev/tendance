84d995fdbf98bdd4b9adfb7eb91cf8a7f35ca77f
fapethedev <mpetrivlin@gmail.com>
2024-04-24 13:46:18 +0000

    [ADD] dto for contact form processing

---
+++ src/main/java/com/fapethedev/tendance/main/dto/ContactDto.java
@@ -0,0 +1,32 @@
+package com.fapethedev.tendance.main.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+/**
+ * Global exception for entities
+ * @author Fapethedev
+ * @since 1.0
+ */
+@Data
+public class ContactDto
+{
+    @NotNull
+    @NotBlank(message = "Veuillez spécifier le contenu de votre message")
+    private String message;
+
+    @NotNull
+    @NotBlank(message = "Votre nom ne peut pas être laissez vide")
+    private String name;
+
+    @NotNull
+    @NotBlank(message = "Veuillez saisir le sujet de votre message")
+    private String subject;
+
+    @NotNull
+    @Email(message = "Veuillez entrer une adresse email valide")
+    @NotBlank(message = "Veuillez saisir votre adresse email")
+    private String email;
+}
