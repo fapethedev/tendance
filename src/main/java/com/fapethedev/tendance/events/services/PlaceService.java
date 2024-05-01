628de66f03b629b6cb42248d11ede8b2916257bf
fapethedev <mpetrivlin@gmail.com>
2024-05-01 16:21:09 +0000

    [ADD] PlaceService Implementing IPlaceService

---
+++ src/main/java/com/fapethedev/tendance/events/services/PlaceService.java
@@ -0,0 +1,93 @@
+package com.fapethedev.tendance.events.services;
+
+import com.fapethedev.tendance.events.entities.Place;
+import com.fapethedev.tendance.events.form.PlaceForm;
+import com.fapethedev.tendance.events.repository.PlaceRepository;
+import com.fapethedev.tendance.main.exception.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Fapethedev
+ * @since 1.0
+ * @see com.fapethedev.tendance.events.services.IPlaceService
+ * @see com.fapethedev.tendance.main.services.IService
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PlaceService implements IPlaceService
+{
+    private final PlaceRepository placeRepository;
+
+    @Override
+    public Place save(Place place)
+    {
+        log.info("Saving Place");
+
+        return placeRepository.save(place);
+    }
+
+    @Override
+    public Place save(PlaceForm placeForm)
+    {
+        log.info("Adding new Place by using place form");
+
+        Place place = new Place();
+        place.setName(placeForm.getName());
+        place.setLatitude(place.getLatitude());
+        place.setLongitude(place.getLongitude());
+        place.setCountry(placeForm.getCountry());
+        place.setCity(placeForm.getCity());
+        place.setTown(placeForm.getTown());
+        place.setScope(placeForm.getScope());
+
+        return placeRepository.save(place);
+    }
+
+    @Override
+    public Place delete(Place place)
+    {
+        log.info("Deleting Place " + place.getName() + ", with id : " + place.getId());
+
+        placeRepository.delete(place);
+
+        return place;
+    }
+
+    @Override
+    public Place deleteById(UUID uuid)
+    {
+        log.info("Deleting Place with id : " + uuid);
+
+        return delete(findById(uuid));
+    }
+
+    @Override
+    public Place findById(UUID uuid)
+    {
+        log.info("Fetching Place with id : " + uuid);
+
+        return placeRepository.findById(uuid)
+                .orElseThrow(() -> new EntityNotFoundException("Place with id : " + uuid + " not found"));
+    }
+
+    @Override
+    public List<Place> findAll()
+    {
+        return placeRepository.findAll();
+    }
+
+    @Override
+    public Place findByName(String name)
+    {
+        log.info("Fetching Place with name : " + name);
+
+        return placeRepository.findByNameLikeIgnoreCase(name)
+                .orElseThrow(() -> new EntityNotFoundException("Place with name : " + name + " not found"));
+    }
+}
