755fab08700f92819d8fc5703e80bab0d4ee80c0
fapethedev <mpetrivlin@gmail.com>
2024-05-23 18:08:44 +0000

    [UPDATE] AccountService change CrudService for IService

--- src/main/java/com/fapethedev/tendance/users/services/AccountService.java
+++ src/main/java/com/fapethedev/tendance/users/services/AccountService.java
@@ -1,35 +1,47 @@
 package com.fapethedev.tendance.users.services;
 
+import com.fapethedev.tendance.main.services.IService;
 import com.fapethedev.tendance.users.entities.Account;
 import com.fapethedev.tendance.users.form.AccountForm;
 import com.fapethedev.tendance.users.repositories.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 /**
+ * <p>A service layer class for doing crud operations
+ * on Account entity by using AccountForm.</p>
+ *
+ * @see com.fapethedev.tendance.main.services.IService
+ *
  * @author Fapethedev
  * @version 1.0
- *
- * A service layer class for doing crud operations
- * on Account entity by using AccountForm
  */
 @Service
-public class AccountService implements CrudService<Account, AccountForm>
+@Slf4j
+@RequiredArgsConstructor
+public class AccountService implements IService<Account, UUID, AccountForm>
 {
     private final AccountRepository accountRepository;
 
-    @Autowired
-    public AccountService(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
+    @Override
+    public Account save(Account account)
+    {
+        log.info("Saving account");
+
+        return accountRepository.save(account);
     }
 
     @Override
     public Account save(AccountForm accountForm)
     {
-        Account account = Account.builder()
+        log.info("Saving account");
+
+        var account = Account.builder()
                 .picture(accountForm.getPicture())
                 .idProof(accountForm.getIdProof())
                 .bio(accountForm.getBio())
@@ -44,17 +56,9 @@ public class AccountService implements CrudService<Account, AccountForm>
     }
 
     @Override
-    public Account delete(AccountForm accountForm)
+    public Account delete(Account account)
     {
-        Account account = Account.builder()
-                .picture(accountForm.getPicture())
-                .idProof(accountForm.getIdProof())
-                .bio(accountForm.getBio())
-                .user(accountForm.getUser())
-                .active(accountForm.isActive())
-                .build();
-
-        account.setId(accountForm.getId());
+        log.info("Deleting account");
 
         accountRepository.delete(account);
 
@@ -62,11 +66,20 @@ public class AccountService implements CrudService<Account, AccountForm>
     }
 
     @Override
-    public Account findById(AccountForm accountForm)
+    public Account deleteById(UUID uuid)
     {
-        Optional<Account> optionalAccount = accountRepository.findById(accountForm.getId());
+        log.info("Deleting account");
 
-        return null;
+        return delete(findById(uuid));
+    }
+
+    @Override
+    public Account findById(UUID uuid)
+    {
+        log.info("Finding account with id : " + uuid);
+
+        return accountRepository.findById(uuid)
+                .orElseThrow(() -> new EntityNotFoundException("Account not found with id : " + uuid));
     }
 
     @Override
