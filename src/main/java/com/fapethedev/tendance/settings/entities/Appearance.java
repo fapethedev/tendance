591a1d06c1d1395a8ac476a511ff9dbabb8f69e4
fapethedev <mpetrivlin@gmail.com>
2024-05-23 20:46:09 +0000

    [UPDATE] Appearance add annotation

--- src/main/java/com/fapethedev/tendance/settings/entities/Appearance.java
+++ src/main/java/com/fapethedev/tendance/settings/entities/Appearance.java
@@ -2,6 +2,8 @@ package com.fapethedev.tendance.settings.entities;
 
 import com.fapethedev.tendance.main.entities.BaseEntity;
 import com.fapethedev.tendance.users.entities.User;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -13,15 +15,41 @@ import java.util.UUID;
  * @author <a href="https://github.com/fapethedev">Fapethedev</a>
  * @version 1.0
  */
+@Entity
+@Table(name = "appearances")
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Appearance extends BaseEntity<UUID>
 {
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Theme theme;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Direction direction;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Color color;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Layout layout;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Sizing sizing;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Border border;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Card card;
 
+    @Column
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 }
