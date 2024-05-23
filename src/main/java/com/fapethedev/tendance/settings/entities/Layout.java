c8b2582c57f9c663314d503b62c2442ac98262d8
fapethedev <mpetrivlin@gmail.com>
2024-05-23 19:01:43 +0000

    [ADD] Layout

---
+++ src/main/java/com/fapethedev/tendance/settings/entities/Layout.java
@@ -0,0 +1,21 @@
+package com.fapethedev.tendance.settings.entities;
+
+/**
+ * <p>Elements layout disposition on the ui.
+ * They can be dispose horizontally or vertically.</p>
+ *
+ * @author Fapethedev
+ * @version 1.0
+ */
+public enum Layout
+{
+    /**
+     * Vertical layout.
+     */
+    VERTICAL,
+
+    /**
+     * Horizontal layout.
+     */
+    HORIZONTAL
+}
