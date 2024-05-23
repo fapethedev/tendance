742a35d405b5abdd45a860c1b21b7ddef258cfbd
fapethedev <mpetrivlin@gmail.com>
2024-05-23 18:58:05 +0000

    [ADD] Color  ui color

---
+++ src/main/java/com/fapethedev/tendance/settings/entities/Color.java
@@ -0,0 +1,17 @@
+package com.fapethedev.tendance.settings.entities;
+
+/**
+ * <p>UI dominant color like button color, table underline and more.</p>
+ *
+ * @author Fapethedev
+ * @version 1.0
+ */
+public enum Color
+{
+    BLUE,
+    AQUA,
+    PURPLE,
+    GREEN,
+    CYAN,
+    ORANGE
+}
