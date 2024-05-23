e7b2b3bc1b6d92d8cc0262f6b0abfdc2cd813ca8
fapethedev <mpetrivlin@gmail.com>
2024-05-23 12:44:28 +0000

    [UPDATE] ApplicationAsynchronousConfiguration AsynchronousSpringEventsConfig become ApplicationAsynchronousConfiguration and enable async through the application

--- src/main/java/com/fapethedev/tendance/main/configuration/ApplicationAsynchronousConfiguration.java
+++ src/main/java/com/fapethedev/tendance/main/configuration/ApplicationAsynchronousConfiguration.java
@@ -2,23 +2,32 @@ package com.fapethedev.tendance.main.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
- * <p>{@code AsynchronousSpringEventsConfig} is a main configuration class for enable async event on the application.</p>
+ * <p>{@code ApplicationAsynchronousConfiguration} is the
+ * configuration class for enable asynchronous processing in the application .</p>
+ *
  * @author Fapethedev
  * @version 1.0
  */
+@EnableAsync
 @Configuration
-public class AsynchronousSpringEventsConfig
+public class ApplicationAsynchronousConfiguration
 {
     /**
-     * <p>Bean method that will return an instance of {@code ApplicationEventMulticaster} when
-     * an application event needed to be process.</p>
-     * @return ApplicationEventMulticaster implementation
+     * <p>Bean method that will return an instance of {@code ApplicationEventMulticaster}
+     * for enabling asynchronous processing for application event and
+     * {@code AbstractApplicationBaseEvent} realizations.</p>
+     *
+     * @return {@code ApplicationEventMulticaster} implementation
+     * which is inject when a bean is required for asynchronous events processing
      */
+    @Lazy
     @Bean(name = "applicationEventMulticaster")
     public ApplicationEventMulticaster simpleApplicationEventMulticaster()
     {
