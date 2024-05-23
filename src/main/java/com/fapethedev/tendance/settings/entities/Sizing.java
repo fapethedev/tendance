26af0e33a09887bc1c5b0a4d476be9c06650918c
fapethedev <mpetrivlin@gmail.com>
2024-05-23 19:11:11 +0000

    [ADD] Sizing

---
+++ src/main/java/com/fapethedev/tendance/settings/entities/Sizing.java
@@ -0,0 +1,21 @@
+package com.fapethedev.tendance.settings.entities;
+
+/**
+ * <p>Elements sizing.</p>
+ *
+ * @author <a href="www.github.com/fapethedev/">Fapethedev</a>
+ * @version 1.0
+ */
+public enum Sizing
+{
+    /**
+     * Elements are boxed, so they don't take all
+     * spaces available for them.
+     */
+    BOXED,
+
+    /**
+     * Elements are fully take available space.
+     */
+    FULL
+}
