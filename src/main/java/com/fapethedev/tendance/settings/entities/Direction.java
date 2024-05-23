41236a372689059d49554e02041846c7ab03c10a
fapethedev <mpetrivlin@gmail.com>
2024-05-23 18:55:21 +0000

    [ADD] Direction  text direction

---
+++ src/main/java/com/fapethedev/tendance/settings/entities/Direction.java
@@ -0,0 +1,20 @@
+package com.fapethedev.tendance.settings.entities;
+
+/**
+ * <p>Text direction on the ui.
+ * <br/>
+ * It could take value like LTR(Left To Right) or RTL(Right To Left).
+ * <p/>
+ */
+public enum Direction
+{
+    /**
+     * Left To Right text direction.
+     */
+    LTR,
+
+    /**
+     * Right To Left text direction.
+     */
+    RTL
+}
