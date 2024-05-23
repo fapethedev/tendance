153a8d339695f6b68ee115978efb79058ea0700b
fapethedev <mpetrivlin@gmail.com>
2024-05-23 18:49:48 +0000

    [ADD] Theme theme mode for the application

---
+++ src/main/java/com/fapethedev/tendance/settings/entities/Theme.java
@@ -0,0 +1,21 @@
+package com.fapethedev.tendance.settings.entities;
+
+/**
+ * <p>Theme available for the application ui
+ * .It's also call color mode.</p>
+ *
+ * @author Fapethedev
+ * @version 1.0
+ */
+public enum Theme
+{
+    /**
+     * The light theme
+     */
+    LIGHT,
+
+    /**
+     * The dark theme
+     */
+    DARK
+}
