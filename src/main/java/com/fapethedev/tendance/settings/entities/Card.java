02d15ef98e3d52e5ace5cbdc9b04da12234675e6
fapethedev <mpetrivlin@gmail.com>
2024-05-23 19:14:16 +0000

    [ADD] Card Card container border decoration.

---
+++ src/main/java/com/fapethedev/tendance/settings/entities/Card.java
@@ -0,0 +1,21 @@
+package com.fapethedev.tendance.settings.entities;
+
+/**
+ * <p>Card container border decoration.</p>
+ *
+ * @author <a href="www.github.com/fapethedev/">Fapethedev</a>
+ * @version 1.0
+ */
+public enum Card
+{
+    /**
+     * The container is just bordered.
+     */
+    BORDERED,
+
+    /**
+     * The container is bordered and also has
+     * a shadow.
+     */
+    SHADOW
+}
