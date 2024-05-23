8b2fee61349a5c72c131219194ae0683794dd13d
fapethedev <mpetrivlin@gmail.com>
2024-05-23 19:12:25 +0000

    [ADD] Border Elements bordering

---
+++ src/main/java/com/fapethedev/tendance/settings/entities/Border.java
@@ -0,0 +1,13 @@
+package com.fapethedev.tendance.settings.entities;
+
+/**
+ * <p>Elements bordering management.</p>
+ *
+ * @author <a href="www.github.com/fapethedev/">Fapethedev</a>
+ * @version 1.0
+ */
+public enum Border
+{
+    FULL,
+    MINIMIZED
+}
