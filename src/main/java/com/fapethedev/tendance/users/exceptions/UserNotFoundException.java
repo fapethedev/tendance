56c0cd5cea36f10049be5da2c410aa43568a7867
fapethedev <mpetrivlin@gmail.com>
2024-05-23 18:05:44 +0000

    [UPDATE] UserNotFoundException add documentation

--- src/main/java/com/fapethedev/tendance/users/exceptions/UserNotFoundException.java
+++ src/main/java/com/fapethedev/tendance/users/exceptions/UserNotFoundException.java
@@ -1,6 +1,19 @@
 package com.fapethedev.tendance.users.exceptions;
 
-public class UserNotFoundException extends RuntimeException {
+import jakarta.persistence.EntityNotFoundException;
+
+/**
+ * <p>Specialized exception class for User entity if they
+ * was not found when querying the database through jpa.</p>
+ *
+ * @see jakarta.persistence.EntityNotFoundException
+ * @see jakarta.persistence.PersistenceException
+ *
+ * @author Faoethedev
+ * @version 1.0
+ */
+public class UserNotFoundException extends EntityNotFoundException
+{
     public UserNotFoundException(String message) {
         super(message);
     }
