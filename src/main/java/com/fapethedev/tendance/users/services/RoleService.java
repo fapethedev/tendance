a85967645b4a1bae8db244a954dfebf4ae3667bc
fapethedev <mpetrivlin@gmail.com>
2024-05-24 19:09:41 +0000

    [UPDATE] RoleService  Logging and contract are done.

--- src/main/java/com/fapethedev/tendance/users/services/RoleService.java
+++ src/main/java/com/fapethedev/tendance/users/services/RoleService.java
@@ -6,8 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
+/**
+ * <p>IRoleService default implementation.</p>
+ *
+ * @see com.fapethedev.tendance.users.services.IRoleService
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
 @Service
 @Slf4j
 public class RoleService implements IRoleService
@@ -20,12 +26,30 @@ public class RoleService implements IRoleService
     }
 
     @Override
-    public Role save(Role role) {
+    public Role save(Role role)
+    {
+        log.info("Saving role " + role.getName());
+
         return repository.save(role);
     }
 
     @Override
-    public Optional<Role> findByName(String name) {
-        return repository.findRoleByName(name);
+    public Role findByName(String name)
+    {
+        log.info("Finding role by name: " + name);
+
+        return repository.findRoleByName(name).orElseGet(() ->
+        {
+            log.warn("Role with name " + name + " not found");
+            log.warn("'Finding the default role");
+
+            return repository.findRoleByName(Role.Category.ROLE_STANDARD.name())
+                    .orElseGet(() -> {
+                        log.warn("Default role not find");
+                        log.warn("Creating default role");
+
+                        return save(new Role(Role.Category.ROLE_STANDARD));
+                    });
+        });
     }
 }
