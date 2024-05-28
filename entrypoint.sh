092d936ad664176a24f384262560d8f90adf99b6
fapethedev <mpetrivlin@gmail.com>
2024-05-28 21:26:12 +0000

    [ADD] entrypoint.sh shell script for exec the app in docker images

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# port: 8080
+java -Xms128M \
+	 -XX:MaxGCPauseMillis=500 \
+	 -XX:+UseSerialGC \
+     -XX:+UseCompressedOops \
+     -XX:+OptimizeStringConcat \
+	 -XX:NewRatio=3 \
+	 -XX:ParallelGCThreads=8 \
+	 -XX:InitiatingHeapOccupancyPercent=30 \
+	 -jar bot.jar
