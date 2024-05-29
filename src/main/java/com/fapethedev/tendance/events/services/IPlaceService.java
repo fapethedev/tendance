07c442550ed284b6c7eeeb7b8e8dbf6bf7b3e75b
fapethedev <mpetrivlin@gmail.com>
2024-05-29 22:51:10 +0000

    [UPDATE] IPlaceService Modify documentation

--- src/main/java/com/fapethedev/tendance/events/services/IPlaceService.java
+++ src/main/java/com/fapethedev/tendance/events/services/IPlaceService.java
@@ -2,25 +2,29 @@ package com.fapethedev.tendance.events.services;
 
 import com.fapethedev.tendance.events.entities.Place;
 import com.fapethedev.tendance.events.form.PlaceForm;
-import com.fapethedev.tendance.main.exception.EntityNotFoundException;
 import com.fapethedev.tendance.main.services.IService;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.util.UUID;
 
 /**
- * Service Layer for {@code Place}
- * @author Fapethedev
- * @since 1.0
+ * <p>Service layer interface for {@code Place}.</p>
+ *
  * @see com.fapethedev.tendance.main.services.IService
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
  */
 public interface IPlaceService extends IService<Place, UUID, PlaceForm>
 {
     /**
-     * Get a place with the specified name
+     * Get a place with the specified name.
+     *
      * @param name the name of the place
+     *
      * @return The {@code Place} with the given name
-     * @throws com.fapethedev.tendance.main.exception.EntityNotFoundException
-     * if no place with the given found
+     *
+     * @throws EntityNotFoundException if no place with the given found
      */
     Place findByName(String name) throws EntityNotFoundException;
 }
