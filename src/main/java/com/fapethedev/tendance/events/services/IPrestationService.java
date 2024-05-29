35316045f3ff24b870f8f225cc2fc3b8488852f5
fapethedev <mpetrivlin@gmail.com>
2024-05-29 23:02:47 +0000

    [UPDATE] IPrestationService  Adds a new method for prestation creation.

--- src/main/java/com/fapethedev/tendance/events/services/IPrestationService.java
+++ src/main/java/com/fapethedev/tendance/events/services/IPrestationService.java
@@ -1,16 +1,27 @@
 package com.fapethedev.tendance.events.services;
 
 import com.fapethedev.tendance.events.entities.Prestation;
+import com.fapethedev.tendance.events.entities.PrestationRequest;
 import com.fapethedev.tendance.events.form.PrestationForm;
 import com.fapethedev.tendance.main.services.IService;
 
 import java.util.UUID;
 
 /**
- * Service Layer for {@code Prestation}
- * @author Fapethedev
- * @since 1.0
+ * <p>Service layer interface for {@code Prestation}.</p>
+ *
  * @see com.fapethedev.tendance.main.services.IService
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
  */
-public interface IPrestationService extends IService<Prestation, UUID, PrestationForm> {
+public interface IPrestationService extends IService<Prestation, UUID, PrestationForm>
+{
+    /**
+     * <p>Creates a new prestation from an accepted prestation request.<</p>
+     *
+     * @param request the prestation request with the status accepted
+     * @return a valid prestation
+     */
+    Prestation createPrestationFromRequest(PrestationRequest request);
 }
