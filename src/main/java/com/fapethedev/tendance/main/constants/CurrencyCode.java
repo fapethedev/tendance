5233224b3482134230c5fb480ff2cbb70a624825
fapethedev <mpetrivlin@gmail.com>
2024-05-30 00:08:06 +0000

    [ADD] CurrencyCode as constant  Currency for payments and subscription should be constant

---
+++ src/main/java/com/fapethedev/tendance/main/constants/CurrencyCode.java
@@ -0,0 +1,26 @@
+package com.fapethedev.tendance.main.constants;
+
+/**
+ * <p>Enum constant for all available currency in the application
+ * for such operation like subscription or payment.</p>
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
+public enum CurrencyCode
+{
+    /**
+     * <p>XOF is the code of Franc CFA.</p>
+     */
+    XOF,
+
+    /**
+     * <p>The code for US Dollar.</p>
+     */
+    USD,
+
+    /**
+     * <p>The code for Euro.</p>
+     */
+    EUR,
+}
