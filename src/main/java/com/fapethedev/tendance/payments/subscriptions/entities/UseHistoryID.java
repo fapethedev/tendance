419d4c7d5be275bfd34486480fd5115d22c7d2ab
fapethedev <mpetrivlin@gmail.com>
2024-06-01 19:01:15 +0000

    [ADD] UseHistoryID  A use history embedded id

---
+++ src/main/java/com/fapethedev/tendance/payments/subscriptions/entities/UseHistoryID.java
@@ -0,0 +1,29 @@
+package com.fapethedev.tendance.payments.subscriptions.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * <p>The id of {@code UseHistory} entity as it's a join column
+ * from functionalities and users.</p>
+ */
+@Data
+@Embeddable
+public class UseHistoryID implements Serializable
+{
+    /**
+     * <p>The referenced functionality id.</p>
+     */
+    @Column(name = "functionality_id")
+    private UUID functionalityId;
+
+    /**
+     * <p>The referenced user id.</p>
+     */
+    @Column(name = "user_id")
+    private UUID userId;
+}
