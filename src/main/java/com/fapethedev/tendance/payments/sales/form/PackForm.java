c0a46d05851be018c363c1c3a8d6aed545fd0668
fapethedev <mpetrivlin@gmail.com>
2024-06-02 14:03:21 +0000

    [ADD] PackForm form for creating a pack of ticket

---
+++ src/main/java/com/fapethedev/tendance/payments/sales/form/PackForm.java
@@ -0,0 +1,25 @@
+package com.fapethedev.tendance.payments.sales.form;
+
+import com.fapethedev.tendance.payments.sales.entities.Pass;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import java.util.List;
+
+/**
+ * <p>The form for creating a pack of tickets.</p>
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
+@Builder
+public class PackForm extends PassForm
+{
+    /**
+     * <p>The list of tickets that's compose the pass.</p>
+     */
+    @NotNull(message = "La liste des ticket du pack ne doit pas être null")
+    @NotEmpty(message = "La liste des ticket du pack ne doit pas être vide")
+    private List<Pass> passes;
+}
