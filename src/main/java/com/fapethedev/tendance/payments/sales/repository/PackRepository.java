2b51260fb4f93faa3396c380d9b3c1f7bd1a9901
fapethedev <mpetrivlin@gmail.com>
2024-06-02 14:09:22 +0000

    [ADD] PackRepository repo for pack of ticket

---
+++ src/main/java/com/fapethedev/tendance/payments/sales/repository/PackRepository.java
@@ -0,0 +1,17 @@
+package com.fapethedev.tendance.payments.sales.repository;
+
+import com.fapethedev.tendance.payments.sales.entities.Pass;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+/**
+ * <p>Database layer interface for pack of tickets.</p>
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
+@Repository
+public interface PackRepository extends JpaRepository<Pass, UUID> {
+}
