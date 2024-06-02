149c2010e25ab4437f2ff58d1a3b5ffec6615dd5
fapethedev <mpetrivlin@gmail.com>
2024-06-02 11:54:52 +0000

    [ADD] PassType

---
+++ src/main/java/com/fapethedev/tendance/payments/sales/entities/PassType.java
@@ -0,0 +1,25 @@
+package com.fapethedev.tendance.payments.sales.entities;
+
+/**
+ * <p>The type of the pass.</p>
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
+public enum PassType
+{
+    /**
+     * <p>Simple ticket.</p>
+     */
+    SIMPLE,
+
+    /**
+     * <p>Vip ticket.</p>
+     */
+    VIP,
+
+    /**
+     * <p>Reservation ticket.</p>
+     */
+    RESERVATION
+}
