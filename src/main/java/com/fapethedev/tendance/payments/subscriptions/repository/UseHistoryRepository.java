05e024c574faf1dd3ee4a1d678729adf68cffa98
fapethedev <mpetrivlin@gmail.com>
2024-06-02 08:30:52 +0000

    [UPDATE] UseHistoryRepository adds findAllByUserId method

--- src/main/java/com/fapethedev/tendance/payments/subscriptions/repository/UseHistoryRepository.java
+++ src/main/java/com/fapethedev/tendance/payments/subscriptions/repository/UseHistoryRepository.java
@@ -4,15 +4,18 @@ import com.fapethedev.tendance.payments.subscriptions.entities.UseHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * <p>The use history entity database layer interface.</p>
  *
+ * @param <T> the type of user id
+ *
  * @author <a href="www.github.com/fapethedev/">Fapethedev</a>
  * @version 1.0
  */
 @Repository
-public interface UseHistoryRepository extends JpaRepository<UseHistory, UUID> {
-
+public interface UseHistoryRepository<T> extends JpaRepository<UseHistory, UUID> {
+    List<UseHistory> findAllByUserId(T user_id);
 }
