c18bd817d9345e2d15a100fa088d32b32dd52ec6
fapethedev <mpetrivlin@gmail.com>
2024-06-02 14:16:24 +0000

    [ADD] IPackService service layer interface for pack of ticket

---
+++ src/main/java/com/fapethedev/tendance/payments/sales/services/IPackService.java
@@ -0,0 +1,18 @@
+package com.fapethedev.tendance.payments.sales.services;
+
+import com.fapethedev.tendance.main.services.IService;
+import com.fapethedev.tendance.payments.sales.entities.Pass;
+import com.fapethedev.tendance.payments.sales.form.PassForm;
+
+import java.util.UUID;
+
+/**
+ * <p>Service layer interface for pack of ticket.</p>
+ *
+ * @see com.fapethedev.tendance.main.services.IService
+ *
+ * @author <a href="www.github.com/fapethedev/">Fapethedev</a>
+ * @version 1.0
+ */
+public interface IPackService extends IService<Pass, UUID, PassForm> {
+}
