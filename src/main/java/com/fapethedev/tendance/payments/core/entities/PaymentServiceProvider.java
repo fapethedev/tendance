ed8298a05ce39a5c19b1e3278a287f7d0971f2ba
fapethedev <mpetrivlin@gmail.com>
2024-06-03 09:37:12 +0000

    [ADD] PaymentProviderService

---
+++ src/main/java/com/fapethedev/tendance/payments/core/entities/PaymentServiceProvider.java
@@ -0,0 +1,15 @@
+package com.fapethedev.tendance.payments.core.entities;
+
+/**
+ * <p>Payment service provider name.</p>
+ *
+ * @author <a href="www.github.com/fapethedev/">Fapethedev</a>
+ * @version 1.0
+ */
+public enum PaymentServiceProvider
+{
+    /**
+     * <p>Test provider as no use for prod just demos.</p>
+     */
+    UNKNOWN,
+}
