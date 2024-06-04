e9c62f51c75d547fe25b516084ac87ba1b52c58b
fapethedev <mpetrivlin@gmail.com>
2024-06-04 15:00:45 +0000

    [CHANGES] Overrides default configuration

--- src/main/java/com/fapethedev/tendance/main/configuration/ApplicationSessionConfiguration.java
+++ src/main/java/com/fapethedev/tendance/main/configuration/ApplicationSessionConfiguration.java
@@ -1,7 +1,9 @@
 package com.fapethedev.tendance.main.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
@@ -17,13 +19,22 @@ import org.springframework.session.web.context.AbstractHttpSessionApplicationIni
 public class ApplicationSessionConfiguration extends AbstractHttpSessionApplicationInitializer
 {
     /**
+     * <p>Redis instance url</p>
+     */
+    @Value("${spring.data.redis.url}")
+    private String url;
+    
+    /**
      * Creates a new connection with redis factory.
+     * Use the server url to create a redis configuration overriding
+     * default configuration for the redis connection.
      *
-     * @return a redis session factory instance
+     * @return a redis session factory instance with the redis server url
      */
     @Bean
+    @Lazy
     public LettuceConnectionFactory connectionFactory()
     {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(LettuceConnectionFactory.createRedisConfiguration(url));
     }
 }
