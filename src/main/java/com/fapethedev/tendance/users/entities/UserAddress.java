92f10e364e38af427d098bfc9aec423013ccad89
fapethedev <mpetrivlin@gmail.com>
2024-06-15 21:07:57 +0000

    [UPDATE] UserAddress.java by respecting the typo and adding a doc

--- src/main/java/com/fapethedev/tendance/users/entities/UserAddress.java
+++ src/main/java/com/fapethedev/tendance/users/entities/UserAddress.java
@@ -9,6 +9,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+/**
+ * <p>Embedded object that's represent the address of a user which is defined by
+ * the user phone number, his resident country and city.Since the implementation of OAuth2 protocol
+ * this is nullable entirely and only the phone number is asked when the login with the form is used.
+ * </p>
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
 @Embeddable
 @Data
 @Builder
@@ -34,11 +45,20 @@ import lombok.NoArgsConstructor;
                 )
         ),
 })
-public class UserAdress
+public class UserAddress implements Serializable
 {
+    /**
+     * <p>The user phone number.</p>
+     */
     private String phone;
 
+    /**
+     * <p>The user resident city.</p>
+     */
     private String city;
 
+    /**
+     * <p>The user resident country.</p>
+     */
     private String country;
 }
