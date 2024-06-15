ad2f5b467a1e1b9e88020feeddf218a35c6a5d34
fapethedev <mpetrivlin@gmail.com>
2024-06-15 21:00:27 +0000

    [UPDATE] UserIdentity by adding a useful doc

--- src/main/java/com/fapethedev/tendance/users/entities/UserIdentity.java
+++ src/main/java/com/fapethedev/tendance/users/entities/UserIdentity.java
@@ -9,9 +9,24 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+/**
+ * <p>Embedded object that's represent the identity of a user which is defined by
+ * the user lastname, firstname and his email.Since user can register with OAuth2 protocol
+ * the lastname and firstname become nullable field because some provider don't give these
+ * informations.</p>
+ *
+ * @see User
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
 @Embeddable
 @Data
-@Builder @NoArgsConstructor @AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @AttributeOverrides({
         @AttributeOverride(
                 name = "lastname",
@@ -32,11 +47,21 @@ import lombok.NoArgsConstructor;
                 )
         ),
 })
-public class UserIdentity
+public class UserIdentity implements Serializable
 {
+    /**
+     * <p>The lastname of the user.Can be null.</p>
+     */
     private String lastname;
 
+    /**
+     * <p>The firstname of the user. Can be null.</p>
+     */
     private String firstname;
 
+    /**
+     * <p>The user email address which is the only required
+     * field here and are unique for every user.</p>
+     */
     private String email;
 }
