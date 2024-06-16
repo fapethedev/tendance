49b5da0ba59376f3a82724c28e788af361a7f8f9
fapethedev <mpetrivlin@gmail.com>
2024-06-16 09:20:57 +0000

    [ADD] Exception for cdn ops

---
+++ src/main/java/com/fapethedev/tendance/main/exception/CdnException.java
@@ -0,0 +1,29 @@
+package com.fapethedev.tendance.main.exception;
+
+/**
+ * <p>Cdn relative operation failure exception.</p>
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
+public class CdnException extends RuntimeException
+{
+    /**
+     * <p>Constructor matching super with string param.</p>
+     *
+     * @param message the exception message.
+     */
+    public CdnException(String message) {
+        super(message) ;
+    }
+
+    /**
+     * <p>Constructor matching super with string param and throwable cause.</p>
+     *
+     * @param message the exception message.
+     * @param cause the exception itself.
+     */
+    public CdnException(String message, Throwable cause) {
+        super(message, cause) ;
+    }
+}
