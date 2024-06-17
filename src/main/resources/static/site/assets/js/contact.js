00c2183c2f532e16d522ebf78cbf853d6f22050a
fapethedev <mpetrivlin@gmail.com>
2024-06-17 21:01:56 +0000

    [UPDATE] contact.js

--- src/main/resources/static/site/assets/js/contact.js
+++ src/main/resources/static/site/assets/js/contact.js
@@ -35,48 +35,24 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
+                    required: "Veuillez mettre votre nom",
+                    minlength: "Le nom doit comporter plus de 2 caractères"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
+                    required: "Il y a bien un sujet dont vous voulez nous parlez n'es-ce-pas?",
+                    minlength: "Le sujet doit comporter plus de deux caractères"
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
+                    required: "Veuillez entrez votre numéro",
+                    minlength: "Le numéro doit contenir minimum 5 chiffres"
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Veuillez spécifier votre email"
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "Il faut bien écrire un message",
+                    minlength: "Dites-nous en plus"
                 }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
             }
         })
     })
