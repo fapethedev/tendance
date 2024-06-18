13963cb28355c96885c0fce1f712ba3048702e43
fapethedev <mpetrivlin@gmail.com>
2024-06-18 21:53:39 +0000

    [UPDATE] IUserService.java a method to save with registerForm as param

--- src/main/java/com/fapethedev/tendance/users/services/IUserService.java
+++ src/main/java/com/fapethedev/tendance/users/services/IUserService.java
@@ -3,29 +3,45 @@ package com.fapethedev.tendance.users.services;
 import com.fapethedev.tendance.main.services.IService;
 import com.fapethedev.tendance.users.entities.User;
 import com.fapethedev.tendance.users.exceptions.UserNotFoundException;
+import com.fapethedev.tendance.users.form.RegisterForm;
 import com.fapethedev.tendance.users.form.UserForm;
 
 import java.util.UUID;
 
 /**
- * Service layer class for {@code User}
- * @author Fapethedev
- * @since 1.0
+ * <p>Service layer interface for crud operation on {@code User} entities.</p>
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
  */
 public interface IUserService extends IService<User, UUID, UserForm>
 {
     /**
-     * Get a user using his email
+     * Get a user using his email.
+     * 
      * @param email the email of the user. This email acts like the username too
+     *
      * @return {@code User} with the corresponding email
+     *
      * @throws UserNotFoundException if no user is found
      */
     User findUserByEmail(String email) throws UserNotFoundException;
 
     /**
-     * Check if a email is associated with a user
+     * Check if an email is associated with a user.
+     *
      * @param email the email of the user.
+     *
      * @return true if a user has this email, false otherwise
      */
     boolean existByEmail(String email);
+
+    /**
+     * <p>Save a new user through a registration form.</p>
+     *
+     * @param form the form filled by the user to register
+     *
+     * @return the new user
+     */
+    User save(RegisterForm form);
 }
