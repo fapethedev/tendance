1bf4ba8e56abf5e20dd15e124bc1dc6670efdc2b
fapethedev <mpetrivlin@gmail.com>
2024-06-18 22:27:38 +0000

    [ADD] MvcRegisterController.java new controller for registration

---
+++ src/main/java/com/fapethedev/tendance/controllers/mvc/MvcRegisterController.java
@@ -0,0 +1,104 @@
+package com.fapethedev.tendance.controllers.mvc;
+
+import com.fapethedev.tendance.security.facade.AuthenticationFacade;
+import com.fapethedev.tendance.users.entities.User;
+import com.fapethedev.tendance.users.form.RegisterForm;
+import com.fapethedev.tendance.users.publisher.UserEventPublisher;
+import com.fapethedev.tendance.users.services.IUserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.UUID;
+
+/**
+ * <p>Model view controller for user registration,
+ * account activation and all relate operations.</p>
+ *
+ * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+ *
+ * @author <a href="https://github.com/fapethedev">Fapethedev</a>
+ * @version 1.0
+ */
+@Controller
+@RequestMapping("/register")
+@RequiredArgsConstructor
+public class MvcRegisterController implements WebMvcConfigurer
+{
+    private final IUserService userService;
+    private final MessageSource i18n;
+    private final AuthenticationFacade facade;
+    private final UserEventPublisher publisher;
+
+    @GetMapping(path = {"", "/", "/{id}", "/{id}/"})
+    public String showRegisterForm(Model model, @PathVariable(required = false) final String id)
+    {
+        RegisterForm userForm = new RegisterForm();
+
+        model.addAttribute("user", userForm);
+
+        return "/dashboard/authentication-register";
+    }
+
+    @PostMapping(path = {"/save/", "/save", "/save{id}", "/save{id}/"})
+    public String saveUser(@Valid @ModelAttribute("user") RegisterForm user,
+                           final BindingResult result,
+                           @PathVariable(required = false) final String id,
+                           final RedirectAttributes attr)
+    {
+
+        if (userService.existByEmail(user.getEmail()))
+        {
+            result.rejectValue("email", "", "Cet Adresse email est déja utilisé");
+        }
+
+        if (result.hasErrors() || result.hasFieldErrors())
+        {
+            attr.addFlashAttribute("title", i18n.getMessage("dash.register.title", null, LocaleContextHolder.getLocale()));
+            attr.addFlashAttribute("user", user);
+            attr.addFlashAttribute("errors", result.getFieldErrors());
+
+            return "/dashboard/authentication-register";
+        }
+
+        User newUser = userService.save(user);
+
+//        publisher.publishUserRegistrationEvent(newUser);
+
+        return "redirect:/register/success/" + newUser.getId();
+    }
+
+    @GetMapping(path = {"/success/{id}", "/success/{id}/"})
+    public String showRegisterSuccessPage(Model model, @PathVariable(required = true) final String id)
+    {
+        User user = userService.findById(UUID.fromString(id));
+
+        model.addAttribute("user", user);
+
+        return "site/activate-account";
+    }
+
+    @GetMapping(path = {"/activate/{id}", "/activate/{id}/"})
+    public String activateAccount(RedirectAttributes attr, @PathVariable(required = true) final String id)
+    {
+        User user = userService.findById(UUID.fromString(id));
+
+        user.getAccount().setActive(true);
+        user.getAccount().setEmailVerified(true);
+
+        user = userService.save(user);
+
+        attr.addFlashAttribute("user", user);
+
+        facade.authWithUsernamePassword(user.getUsername(), user.getPassword(), user.getAuthorities());
+
+        return "redirect:/dashboard/";
+    }
+}
