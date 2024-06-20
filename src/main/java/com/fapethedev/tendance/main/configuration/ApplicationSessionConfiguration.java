0f41638549aa78d4b2058261b6fd137a2f4b0f28
fapethedev <mpetrivlin@gmail.com>
2024-06-20 11:19:31 +0000

    [UPDATE] Enable ssl in redis configuration to allow connection from remote instance with ssl

--- src/main/java/com/fapethedev/tendance/main/configuration/ApplicationSessionConfiguration.java
+++ src/main/java/com/fapethedev/tendance/main/configuration/ApplicationSessionConfiguration.java
@@ -1,12 +1,18 @@
 package com.fapethedev.tendance.main.configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+import java.time.Duration;
 
 /**
  * <p>Configuration for session storage using Redis as an application cache.</p>
@@ -15,6 +21,8 @@ import org.springframework.session.web.context.AbstractHttpSessionApplicationIni
  * @version 1.0
  */
 @Configuration
+@ConfigurationProperties(prefix = "redis")
+@Getter @Setter
 @EnableRedisHttpSession
 public class ApplicationSessionConfiguration extends AbstractHttpSessionApplicationInitializer
 {
@@ -23,7 +31,17 @@ public class ApplicationSessionConfiguration extends AbstractHttpSessionApplicat
      */
     @Value("${spring.data.redis.url}")
     private String url;
-    
+
+    /**
+     * <p>If the redis connection is use ssl or not.</p>
+     */
+    private boolean useSSL;
+
+    /**
+     * <p>Redis command timeout value</p>
+     */
+    private int timeout;
+
     /**
      * Creates a new connection with redis factory.
      * Use the server url to create a redis configuration overriding
@@ -35,6 +53,14 @@ public class ApplicationSessionConfiguration extends AbstractHttpSessionApplicat
     @Lazy
     public LettuceConnectionFactory connectionFactory()
     {
-        return new LettuceConnectionFactory(LettuceConnectionFactory.createRedisConfiguration(url));
+        return useSSL ?
+                new LettuceConnectionFactory(
+                        LettuceConnectionFactory.createRedisConfiguration(url),
+                        LettuceClientConfiguration.builder()
+                                .commandTimeout(Duration.ofMillis(timeout))
+                                .useSsl()
+                                .build()
+                        ) :
+                new LettuceConnectionFactory(LettuceConnectionFactory.createRedisConfiguration(url));
     }
 }
